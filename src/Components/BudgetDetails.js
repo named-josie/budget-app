9bdfaf93ceb0749e86c8a6ed16603ac6154bd7c5
named.josie <named.josie@gmail.com>
2022-07-25 16:55:20 -0400

    updated

--- src/Components/BudgetDetails.js
+++ src/Components/BudgetDetails.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL
 
 export default function BudgetDetails() {
+  
+  
   return (
     <div>
          {/* <Budget /> */}
